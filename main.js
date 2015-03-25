3026cbcd3cfa95d1237a7d454756adaa8fd80c30
Christian <cbulacan9@gmail.com>
2015-03-24 23:36:28 -0400

    first refactor

--- main.js
+++ main.js
@@ -1,113 +1,111 @@
 (function(){
 
 	window.Form = function() {
-
-		var self = this;
-		var submission = [];
-
 		// FORM ELEMENTS
-		self.form = document.createElement("form");
-		self.listInput = document.createElement("input");
-		self.list = document.createElement("ul");
-		self.addButton = document.createElement("button");
-		self.submitButton = document.createElement("button");
+		this.form = document.createElement("form");
+		this.listInput = document.createElement("input");
+		this.listItems = document.createElement("ul");
+		this.addButton = document.createElement("button");
+		this.submitButton = document.createElement("button");
+	}
 
-		//ELEMENT PROPERTIES
+	Form.prototype.create = function() {
 		var addText = document.createTextNode("+")
 		var submitText = document.createTextNode("Submit")
+		//ELEMENT PROPERTIES
+		this.form.setAttribute('action', '/');
+		this.form.setAttribute('method', 'POST');
+		this.listInput.setAttribute("class", "add-input");
+		this.addButton.appendChild(addText);
+		this.addButton.setAttribute("type", "button");
+		this.addButton.setAttribute("class", "add");
+		this.submitButton.appendChild(submitText);
+		this.submitButton.setAttribute("type", "submit");
+		this.submitButton.setAttribute("class", "submit");
+		this.listInput.setAttribute("placeholder", "Enter list item here.");
+		//ADD TO PAGE
+		var listSection = document.getElementById('list-section');
+		var form = listSection.appendChild(this.form);
+		form.appendChild(this.listInput);
+		form.appendChild(this.addButton);
+		form.appendChild(this.listItems);
+		form.appendChild(this.submitButton);
+		// FUNCTIONALITY
+		this.listInput.addEventListener("keypress", this.add.bind(this));
+		this.addButton.addEventListener("click", this.add.bind(this));
+		this.submitButton.addEventListener("click", this.submit.bind(this));
+		this.form.addEventListener("click", this.focus.bind(this));
+	}
 
-		self.form.setAttribute('action', '/');
-		self.form.setAttribute('method', 'POST');
-		self.listInput.setAttribute("class", "add-input");
-		self.addButton.appendChild(addText);
-		self.addButton.setAttribute("type", "button");
-		self.addButton.setAttribute("class", "add");
-		self.submitButton.appendChild(submitText);
-		self.submitButton.setAttribute("type", "submit");
-		self.submitButton.setAttribute("class", "submit");
-		self.listInput.setAttribute("placeholder", "Enter list item here.");
-
-		self.listInput.addEventListener("keypress", function(e) {
-			if(e.keyCode === 13) {
-				e.preventDefault();
-				self.add();
-			}
-		});
-
-		self.addButton.addEventListener("click", function(e) {
-			e.preventDefault();
-			self.add();
-		});
-
-		self.submitButton.addEventListener("click", function(e) {
-			e.preventDefault();
-			self.submit(e);
-		});
-		// FORM METHODS
-		self.create = function() {
-			var listSection = document.getElementById('list-section');
-			var form = listSection.appendChild(self.form);
-			form.appendChild(self.listInput);
-			form.appendChild(self.addButton);
-			form.appendChild(self.list);
-			form.appendChild(self.submitButton);
-		}
-
-		self.add = function() {
-			var itemText = self.listInput.value;
-
-			if(itemText.length > 0) {
-				var itemInput = document.createElement("input");
-				var listItem = document.createElement("li");
-				var removeButton = document.createElement("button");
-				var removeText = document.createTextNode("x");
-				removeButton.setAttribute("type", "button");
-				removeButton.setAttribute("class", "remove-item");
-				itemInput.setAttribute("class", "list-item");
-				itemInput.setAttribute("type", "text");
-				itemInput.setAttribute("name", "item[]");
-				itemInput.setAttribute("value", itemText);
-				itemInput.setAttribute("readonly", "readonly");
-
-				removeButton.appendChild(removeText);
-				listItem.appendChild(removeButton);
-				listItem.appendChild(itemInput);
-				self.list.appendChild(listItem);
-
-				self.listInput.value = "";
-				removeButton.addEventListener("click", self.remove);
-			} else {
-				alert('Please enter a something!');
-			}
-		}
-
-		self.remove = function(e) {
-			e.preventDefault();
-			var listItem = e.toElement.parentNode;
-			listItem.parentNode.removeChild(listItem);
-		}
-
-		self.submit = function(e) {
-			var form = e.toElement.parentNode;
-			var listItems = e.toElement.form.children[2].children;
-			if(listItems.length > 0) {
-				localStorage.setItem()
-				form.submit();
-			} else {
-				alert('Nothing to submit!');
-			}
-		}
-
-		self.form.addEventListener("click", function(e) {
-			if(self.form.getAttribute('class') == null) {
-				var forms = document.getElementsByTagName('form');
-				for(i=0; i < forms.length; i++) {
-					forms[i].removeAttribute('class');
+	Form.prototype.add = function(e) {
+		switch(e.type) {
+			case 'keypress':
+				if(e.keyCode === 13) {
+					e.preventDefault();
+					this.list();
 				}
-				self.form.setAttribute('class', 'selected');
+				break;
+			case 'click':
+				this.list();
+				break;
+		}
+	}
+
+	Form.prototype.list = function(e) {
+		var itemText = this.listInput.value;
+		if(itemText.length > 0) {
+			// CREATE ELEMENTS
+			var itemInput = document.createElement("input");
+			var listItem = document.createElement("li");
+			var removeButton = document.createElement("button");
+			var removeText = document.createTextNode("x");
+			// SET ATTRIBUTES
+			removeButton.setAttribute("type", "button");
+			removeButton.setAttribute("class", "remove-item");
+			itemInput.setAttribute("class", "list-item");
+			itemInput.setAttribute("type", "text");
+			itemInput.setAttribute("name", "item[]");
+			itemInput.setAttribute("value", itemText);
+			itemInput.setAttribute("readonly", "readonly");
+			// ADD TO PAGE
+			removeButton.appendChild(removeText);
+			listItem.appendChild(removeButton);
+			listItem.appendChild(itemInput);
+			this.listItems.appendChild(listItem);
+			// FUNCTIONALITY
+			this.listInput.value = "";
+			removeButton.addEventListener("click", this.remove);
+		} else {
+			alert('Please enter a something!');
+		}
+	}
+
+	Form.prototype.remove = function(e) {
+		e.preventDefault();
+		var listItem = e.toElement.parentNode;
+		listItem.parentNode.removeChild(listItem);
+	}
+
+	Form.prototype.focus = function(e) {
+		if(this.form.getAttribute('class') == null) {
+			var forms = document.getElementsByTagName('form');
+			for(i=0; i < forms.length; i++) {
+				forms[i].removeAttribute('class');
 			}
-		})
-	} 	
+			this.form.setAttribute('class', 'selected');
+		}
+	}
+
+	Form.prototype.submit = function(e) {
+		e.preventDefault();
+		var form = e.toElement.parentNode;
+		var listItems = e.toElement.form.children[2].children;
+		if(listItems.length > 0) {
+			form.submit();
+		} else {
+			alert('Nothing to submit!');
+		}
+	}
 
 	var newButton = document.getElementById("new-list");
 	newButton.addEventListener("click", function(e) {
